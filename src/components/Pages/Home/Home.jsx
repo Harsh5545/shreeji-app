fb4539e50401fe01fefba3ff9b1dd0ead961c309
sanjay <Bhavishm009@gmail.com>
2024-02-17 00:58:02 +0530

    changes in home page added some font and cards

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
+import CustomCard from "../../CustomCard/CustomCard";
+import MainComponent from "../../custom/mainComponent";
 
 function Home() {
   return (
-    <div className='flex bg-blue-500 gap-5'>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto max-w-screen-xl my-5">
+      <CustomCard />
+      <CustomCard />
+      <CustomCard />
+      <CustomCard />
+      <CustomCard />
+      <CustomCard />
+      <CustomCard />
+      <CustomCard />
     </div>
-  )
+    
+  );
 }
 
-export default Home
+export default Home;
