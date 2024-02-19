05035983c45bf8bb113bd3f84e3131ec7a450bbf
sanjay <Bhavishm009@gmail.com>
2024-02-19 20:38:04 +0530

    changes in card

---
+++ src/components/Modal/Model.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Checkbox,
+  Input,
+  Link,
+} from "@nextui-org/react";
+import { MailIcon } from "./MailIcon.jsx";
+import { LockIcon } from "./LockIcon.jsx";
+export default function CustomModel({ isOpen, onOpen, onOpenChange }) {
+  
+
+  return (
+    <>
+      <Modal backdrop={"blur"} isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Contact Us</ModalHeader>
+              <ModalBody>
+                <Input
+                  autoFocus
+                  endContent={
+                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Email"
+                  placeholder="Enter your email"
+                  variant="bordered"
+                />
+                <Input
+                  endContent={
+                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                />
+                <div className="flex py-2 px-1 justify-between">
+                
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
