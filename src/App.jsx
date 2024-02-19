7d2d143a734d264748653466266af57250afa73f
sanjay <Bhavishm009@gmail.com>
2024-02-20 00:43:35 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import AppRouter from "./Routes/AppRouter";
 import SEO from "./SEO/SEO";
 import FooterComponent from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import { DayPicker } from "react-day-picker";
+
 function App() {
   return (
     <>
-      
-        <Navbar />
-        <AppRouter />
-        <FooterComponent/>
+     
+      <Navbar />
+      <AppRouter />
+      <FooterComponent />
     </>
   );
 }
