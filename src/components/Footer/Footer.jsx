c0940a54be382a664a9864b2f4bf109a707caa77
Harsh <harshjaiswal28555@gmail.com>
2024-02-19 18:42:24 +0530

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -207,6 +207,7 @@ export default function FooterComponent() {
         </div>
       </div>
 
+
       {/* <!--Copyright section--> */}
       <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
         <span>© 2023 Copyright:</span>
