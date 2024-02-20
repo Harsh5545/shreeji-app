dfd12ffa1371e9555c2cab09674a935b2baeaf26
sanjay <Bhavishm009@gmail.com>
2024-02-20 18:56:54 +0530

    changes

--- src/components/Modal/Model.jsx
+++ src/components/Modal/Model.jsx
@@ -14,9 +14,71 @@ import {
 import { MailIcon } from "./MailIcon.jsx";
 import { PhoneIcon } from "./PhoneIcon.jsx";
 import { NameIcon } from "./NameIcon.jsx";
+import { apiCall } from "../../Api/Api.js";
+import toast, { Toaster } from "react-hot-toast";
 export default function CustomModel({ isOpen, onOpen, onOpenChange }) {
+  // const notify = () => toast.success("We Will get Back You Soon.");
+
+  const postData = async () => {
+    const apiUrl = "https://api.hdfonline.in/api/v1/sendMail";
+
+    const postData = {
+      mail_info: "value1",
+      user: "chatapp585@gmail.com",
+      password: "zsgklfeiarxcoyac",
+      from: "Bhavishm009@gmail.com",
+      to: "Bhavishm009@gmail.com",
+      subject: "Hello ",
+      description: "Something New",
+      html: "<div>Hello</div>",
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const responseData = await apiCall({
+        url: apiUrl,
+        method: "POST",
+        data: postData,
+        headers: headers,
+      });
+    } catch (error) {
+      console.error("Error in posting data:", error);
+    }
+
+  };
+
+  // const myPromise = postData;
+
+  //   toast.promise(myPromise, {
+  //     loading: "Sending Message...",
+  //     success: "We Will get Back You Soon.",
+  //     error: "Oop's Something Went Wrong..",
+  //   });
+
+    // toast.promise(
+    //   postData,
+    //   {
+    //     loading: "Sending Message...",
+    //     success: "We Will get Back You Soon.",
+    //     error: "Oop's Something Went Wrong..",
+    //   },
+    //   {
+    //     style: {
+    //       minWidth: '250px',
+    //     },
+    //     success: {
+    //       duration: 5000,
+    //       icon: '🔥',
+    //     },
+    //   }
+    // );
+
   return (
     <>
+      <Toaster />
       <Modal
         backdrop={"blur"}
         isOpen={isOpen}
@@ -69,7 +131,7 @@ export default function CustomModel({ isOpen, onOpen, onOpenChange }) {
                 <div className="flex py-2 px-1 justify-between"></div>
               </ModalBody>
               <ModalFooter className="mx-auto roboto-bold">
-                <Button color="success" onPress={onClose}>
+                <Button color="success" onPress={onClose} onClick={postData}>
                   Send Message
                 </Button>
               </ModalFooter>
