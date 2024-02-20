58f0ae6e4d3d2d46fcbbcb11119447ebca6b62a4
harshad <Harshadkajale333@gmail.com>
2024-02-20 20:38:46 +0530

    update

--- src/components/Pages/FoodPackaging/FoodPackaging.jsx
+++ src/components/Pages/FoodPackaging/FoodPackaging.jsx
@@ -13,19 +13,19 @@ function FoodPackaging() {
         }}
       >
         <div className="flex justify-start items-center h-[60vh] w-[45%]">
-          <h2 className="p-1 font-medium text-4xl">
+          <h2 className="p-1 font-medium md:text-4xl text-2xl">
             Grow your cloud kitchen and restaurant food business with packaging
             and box solutions from Turtle Boxes
           </h2>
         </div>
       </div>
       <div className="w-full">
-        <div className="flex justify-center items-center flex-col m-20">
-          <h3 className="p-1 w-[75%] text-center font-medium text-4xl  mb-10">
+        <div className="flex justify-center items-center flex-col md:m-20 m-10">
+          <h3 className="p-1 md:w-[75%] w-[90%] text-center font-medium md:text-4xl text-xl  md:mb-10 mb-5">
             Unwrap the Culinary Delight! Explore Foodie Theme Printed &
             Unprinted Boxes.
           </h3>
-          <p className=" w-[60%] text-center p-1 font-normal  text-md">
+          <p className=" md:w-[60%] w-[80%] text-center p-1 font-normal  md:text-md text-sm">
             Indulge in the exquisite world of gastronomy with our premium rigid
             cardboard boxes crafted to preserve and showcase your delectable
             treats. Whether transporting savoury dishes or delightful desserts,
