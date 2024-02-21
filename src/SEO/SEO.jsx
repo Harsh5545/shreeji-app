68e223757c6c8bb0d40ad797f89b0893757864c9
sanjay <Bhavishm009@gmail.com>
2024-02-21 15:19:49 +0530

    changes in some code

--- src/SEO/SEO.jsx
+++ src/SEO/SEO.jsx
@@ -6,18 +6,18 @@ import { Helmet } from 'react-helmet';
 const SEO = ({ title, description, url, image }) => {
   return (
     <Helmet>
-      {/* SEO meta tags */}
+     
       <title>{title}</title>
       <meta name="description" content={description} />
-
-      {/* Open Graph meta tags */}
+      <meta property="og:image" content={image} />
+     
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={image} />
   
 
-      {/* Twitter meta tags */}
+    
       <meta name="twitter:card" content={image} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
