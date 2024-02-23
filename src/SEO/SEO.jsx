a1a3495443d6f4385afe831928ae3a983f913d11
sanjay <Bhavishm009@gmail.com>
2024-02-23 20:07:20 +0530

    changes

--- src/SEO/SEO.jsx
+++ src/SEO/SEO.jsx
@@ -1,27 +1,20 @@
-// components/SEO.js
+import React from "react";
+import { Helmet } from "react-helmet";
 
-import React from 'react';
-import { Helmet } from 'react-helmet';
-
-const SEO = ({ title, description, url, image }) => {
+const SEO = ({ title, description, image }) => {
   return (
     <Helmet>
-     
+      <meta charSet="utf-8" />
       <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:image" content={image} />
-     
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
+      <meta property="og:type" content="article" />
       <meta property="og:image" content={image} />
-  
+      <meta property="og:url" content={window.location.pathname+window.location.search} />
+      <meta name="twitter:card" content="summary_large_image" />
 
-    
-      <meta name="twitter:card" content={image} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Shreeji Graphic's" />
+      <meta name="twitter:image:alt" content={title} />
     </Helmet>
   );
 };
