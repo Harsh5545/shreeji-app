4f5349cfd8896bc45e9698be307f960e5f87ea26
harshad <Harshadkajale333@gmail.com>
2024-03-02 00:55:14 +0530

    add css

--- src/components/Pages/Home/HomeProcess.jsx
+++ src/components/Pages/Home/HomeProcess.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function HomeProcess() {
   return (
-    <div className="bg-[#F2F0F1]">
-      <div className="mx-auto max-w-screen-xl py-5 px-5 flex flex-col gap-8">
+    <div className=" bg-[#F2F0F1]">
+      <div className="mx-auto  max-w-screen-xl py-10 px-5 flex flex-col gap-10">
         <div className="text-center flex flex-col gap-2">
-            <h2 className="text-3xl">Our Process</h2>
+            <h2 className="text-3xl font-bold text-[#FF6C22]">Our Process</h2>
             <p className="text-lg">5 Easy Steps to Design and Order Packaging Online</p>
         </div>
         <img src="/Main-Images/our-process-desktop.png" alt="" className="hidden md:flex"/>
