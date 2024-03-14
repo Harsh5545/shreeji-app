a4e7ea6097fd79a5c977cc7e50c2dbab984bfcba
harshad <Harshadkajale333@gmail.com>
2024-03-14 17:43:40 +0530

    added

--- src/components/Pages/Home/HomeCarouselComponent.jsx
+++ src/components/Pages/Home/HomeCarouselComponent.jsx
@@ -13,13 +13,13 @@ export default function CarouselCrossfade() {
                 ride="carousel"
                 theme={{
                     indicator:
-                        "mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-black bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none",
+                        "mx-[3px]  box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-black bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none",
                 }}
             >
                 <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
                     <TECarouselItem
                         itemID={1}
-                        className="relative bg-[#E58B93] float-left -mr-[100%] hidden w-full !transform-none transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                        className="relative h-[70vh] bg-[#E58B93] float-left -mr-[100%] hidden w-full !transform-none transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
                     >
                         <div className="flex pb-5 md:flex-row flex-col-reverse">
                             <div
@@ -35,20 +35,20 @@ export default function CarouselCrossfade() {
                                 className={`flex flex-col flex-1 gap-5 items-center `}
                             >
                                 <img
-                                    src="/Main-Images/image_9-removebg-preview.png"
+                                    src="/Main-Images/mainImage.png"
                                     alt="shreejiGraphics Main image"
-                                    className="md:p-10 p-2 "
-                                    width={600}
+                                    className="md:p-10 "
+                                    width={500}
                                 />
                             </div>
                         </div>
                     </TECarouselItem>
                     <TECarouselItem
                         itemID={2}
-                        className="relative bg-lime-300  float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                        className="relative h-full bg-lime-300  float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
                     >
                         <div
-                            className={` flex pb-5 md:flex-row flex-col-reverse`}
+                            className={` flex pb-5 h-[70vh] md:flex-row flex-col-reverse`}
                         >
                             <div
                                 className={`flex flex-col justify-center flex-1 gap-5 items-center `}
@@ -63,17 +63,17 @@ export default function CarouselCrossfade() {
                                 className={`flex flex-col justify-center flex-1 gap-5 items-center `}
                             >
                                 <img
-                                    src="/Main-Images/image_47-removebg-preview.png"
+                                    src="/Main-Images/shreejiWebPage.png"
                                     alt="shreejiGraphics Main image"
-                                    className="md:p-10 p-0"
-                                    width={600}
+                                    className="md:p-10 "
+                                    width={500}
                                 />
                             </div>
                         </div>
                     </TECarouselItem>
                     <TECarouselItem
                         itemID={3}
-                        className="relative bg-[#9ED9DC]  float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
+                        className="relative bg-[#9ED9DC] h-[70vh] float-left -mr-[100%] hidden w-full !transform-none opacity-0 transition-opacity duration-[600ms] ease-in-out motion-reduce:transition-none"
                     >
                         <div
                             className={`flex pb-5 md:flex-row  flex-col-reverse`}
@@ -92,8 +92,8 @@ export default function CarouselCrossfade() {
                                 <img
                                     src="/Main-Images/image_2__1_-removebg-preview.png"
                                     alt="shreejiGraphics.com Main image"
-                                    className="md:p-10 p-2"
-                                    width={600}
+                                    className="md:p-10"
+                                    width={500}
                                 />
                             </div>
                         </div>
