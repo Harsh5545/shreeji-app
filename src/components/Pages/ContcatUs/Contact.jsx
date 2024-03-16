ed3982d3fe14e4ff8899cacf090d0c33381220f7
harshad <Harshadkajale333@gmail.com>
2024-03-16 20:43:58 +0530

    add

--- src/components/Pages/ContcatUs/Contact.jsx
+++ src/components/Pages/ContcatUs/Contact.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import image from "/Product/chocolate/image_1.jpg";
 function Contact() {
   return (
-    <div>Contact</div>
+   <div>
+      <div
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
+          minHeight: "60vh",
+        }}
+      >
+        <div className="p-1 min-h-[60vh] font-semibold text-2xl md:text-6xl flex justify-center items-center">
+          Contact Us
+        </div>
+      </div>
+
+</div>
   )
 }
 
